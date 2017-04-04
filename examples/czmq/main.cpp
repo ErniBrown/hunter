aea4be3e43ff16708b4cd9ec32f5361e91f531f8
Ernesto Varoli <ernesto.varoli@gmail.com>
2017-04-04 12:16:56 +0200

    Improved example

--- examples/czmq/main.cpp
+++ examples/czmq/main.cpp
@@ -3,5 +3,15 @@
 
 int main()
 {
-	return 0;
+	zsock_t *push = zsock_new_push ("inproc://example");
+    zsock_t *pull = zsock_new_pull ("inproc://example");
+    zstr_send (push, "Hello, World");
+
+    char *string = zstr_recv (pull);
+    puts (string);
+    zstr_free (&string);
+
+    zsock_destroy (&pull);
+    zsock_destroy (&push);
+    return 0;
 }
