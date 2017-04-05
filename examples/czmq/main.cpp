b76058ed023b22f7ec747a534aa0ef5f94b2501f
Ernesto Varoli <ernesto.varoli@gmail.com>
2017-04-05 12:43:21 +0200

    Removed tabs

--- examples/czmq/main.cpp
+++ examples/czmq/main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-	zsock_t *push = zsock_new_push ("inproc://example");
+    zsock_t *push = zsock_new_push ("inproc://example");
     zsock_t *pull = zsock_new_pull ("inproc://example");
     zstr_send (push, "Hello, World");
 
